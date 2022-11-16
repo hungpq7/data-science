5379051fd7eddb5148f6a30129b764fdb20f0cc7
hungpq5 <hungpq5@tcbs.com.vn>
2022-11-16 18:02:05 +0700

    Argparse

--- output/demo_argparse.py
+++ output/demo_argparse.py
@@ -7,6 +7,13 @@ parser.add_argument('alpha', type=int, default=1, nargs='?', help='first term')
 parser.add_argument('-b', '--beta', type=int, default=2, nargs='?')
 parser.add_argument('-g', '--gamma', type=int, default=3, nargs='?')
 parser.add_argument('delta', type=int, default=4, nargs='?')
-args = parser.parse_args()
+parser.add_argument('--sigma', action='store_true')
+namespace = parser.parse_args()
 
-print(f'alpha={args.alpha}, beta={args.beta}, gamma={args.gamma}, delta={args.delta}')
+print(
+    f'alpha={namespace.alpha}, '
+    f'beta={namespace.beta}, '
+    f'gamma={namespace.gamma}, '
+    f'delta={namespace.delta}, '
+    f'sigma={namespace.sigma}'
+)
