a7f9d0992a0baac8d1fa1df428dd620a517aeb1a
hungpq7 <hung.pq8596@gmail.com>
2024-05-07 22:25:47 +0700

    -

---
+++ tabular-book/util/functions.py
@@ -0,0 +1,13 @@
+def plotPRChart(yTrue, yProb):
+    from sklearn.metrics import precision_recall_curve
+    precision, recall, threshold = precision_recall_curve(yTrue, yProb)
+    precision = precision[:-1]
+    recall = recall[:-1]
+
+    fig, ax = plt.subplots()
+    ax.plot(threshold, precision)
+    ax.plot(threshold, recall)
+    ax.set_xlabel('Threshold')
+    ax.legend(['Precision', 'Recall'], loc='best')
+    ax.axis('scaled')
+    plt.show()
