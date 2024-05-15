27d8bcbf760a4e3c5822cd64f0243bef1820318d
hungpq7 <hung.pq8596@gmail.com>
2024-05-15 23:33:12 +0700

    -

--- 02-mathematics/util/__init__.py
+++ 02-mathematics/util/__init__.py
@@ -10,4 +10,6 @@ from .alias import (
 
 from .plotting import plot_vectors
 
-from .hypothesis_testing.z_test import MeanZTest, ProportionZTest
+from .hypothesis_testing.z_test import ZTestMean, ZTestProportion
+from .hypothesis_testing.f_test import FTest
+from .hypothesis_testing.t_test import TTestOneSample, TTestIndSample
--- 02-mathematics/util/hypothesis_testing/z_test.py
+++ 02-mathematics/util/hypothesis_testing/z_test.py
@@ -1,8 +1,8 @@
 import numpy as np
 import scipy.stats as stats
-from .base_test import BaseTest
+from .base import BaseTest
 
-class MeanZTest(BaseTest):
+class ZTestMean(BaseTest):
     def __init__(self, x1, var1, x2=None, var2=None, const=0, **kwargs):
         super().__init__(**kwargs)
         self.x1 = np.array(x1)
@@ -19,24 +19,26 @@ class MeanZTest(BaseTest):
             self.objective = 'mean'
     
     def _compute_stats(self):
-        self.n1 = self.x1.size
-        self.mean1 = self.x1.mean()
+        n1 = self.x1.shape[0]
+        mean1 = self.x1.mean()
+        var1 = self.var1
 
         if self.type == '1-sample':
-            self.n2 = np.inf
-            self.mean2 = 0
-            self.var2 = 0
+            n2 = np.inf
+            mean2 = 0
+            var2 = 0
         elif self.type == '2-sample':
-            self.n2 = self.x2.size
-            self.mean2 = self.x2.mean()
+            n2 = self.x2.shape[0]
+            mean2 = self.x2.mean()
+            var2 = self.var2
         
-        se = np.sqrt(self.var1 / self.n1 + self.var2 / self.n2)
-        self.test_stat = (self.mean1 - self.mean2 - self.const) / se
+        se = np.sqrt(var1 / n1 + var2 / n2)
+        self.test_stat = (mean1 - mean2 - self.const) / se
     
     def _set_dist(self):
         self.dist = stats.norm()
 
-class ProportionZTest(BaseTest):
+class ZTestProportion(BaseTest):
     def __init__(self, p1, n1, p2=None, n2=None, const=0, **kwargs):
         super().__init__(**kwargs)
         self.p1 = p1
@@ -53,14 +55,20 @@ class ProportionZTest(BaseTest):
             self.objective = 'p'
     
     def _compute_stats(self):
+        p1 = self.p1
+        n1 = self.n1
+
         if self.type == '1-sample':
-            self.p2 = 0
-            self.n2 = np.inf
+            p2 = 0
+            n2 = np.inf
+        else:
+            p2 = self.p2
+            n2 = self.n2
         
-        se = self.p1 * (1 - self.p1) / self.n1 
-        se += self.p2 * (1 - self.p2) / self.n2
+        se = p1 * (1 - p1) / n1 
+        se += p2 * (1 - p2) / n2
         se = np.sqrt(se)
-        self.test_stat = (self.p1 - self.p2 - self.const) / se
+        self.test_stat = (p1 - p2 - self.const) / se
 
     def _set_dist(self):
         self.dist = stats.norm()
