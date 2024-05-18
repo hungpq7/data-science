fe51b56c9d1f120d45711597dc8874243fd3917a
hungpq7 <hung.pq8596@gmail.com>
2024-05-18 23:51:46 +0700

    -

--- 03-data-manipulation/util/_data.py
+++ 03-data-manipulation/util/_data.py
@@ -1,76 +1,93 @@
 import numpy as np
 import pandas as pd
 
-df_aqua = pd.DataFrame({
-    'Year': pd.Series([2020, 2020, 2020, 2020, 2020, 2020]),
-    'Month name': pd.Series(['Jan', 'Jan', 'Jun', 'Jun', 'Jul', 'Jul']),
-    'Month number': pd.Series([1, 1, 6, 6, 7, 7]),
-    'Product Name': pd.Series(['Fish', 'Shrimp', 'Fish', 'Shrimp', 'Fish', 'Shrimp']),
-    'PROFIT': pd.Series([7415, 3239, 7280, 2007, 3574, 9285]),
-    'Company Name': pd.Series(['Pandas', 'Pandas', 'Pandas', 'Pandas', 'Pandas', 'Pandas'])
-})
+df_aqua = pd.DataFrame(
+    columns=['Year', 'Month name', 'Month number', 'Product Name', 'PROFIT', 'Company Name'],
+    data = [
+        (2020, 'Jan', 1, 'Fish', 7415, 'Pandas'),
+        (2020, 'Jan', 1, 'Shrimp', 3239, 'Pandas'),
+        (2020, 'Jun', 6, 'Fish', 7280, 'Pandas'),
+        (2020, 'Jun', 6, 'Shrimp', 2007, 'Pandas'),
+        (2020, 'Jul', 7, 'Fish', 3574, 'Pandas'),
+        (2020, 'Jul', 7, 'Shrimp', 9285, 'Pandas')
+    ]
+)
 
-df_athlete = pd.DataFrame({
-    'year': [2019, 2019, 2020., 2020, 2020, 2020],
-    'date': ['20191103', '20190812', '20200125', '20200129', '20200412', '20200220'],
-    'time': ['145509', '135433', '214412', '124254', '123349', '233517'],
-    'medal': ['Gold', 'Bronze', 'Silver', 'Bronze', 'Silver', 'Silver'],
-    'name': ['Wayne', 'Robert', 'Ashley', 'Jamie', 'Jessie', 'Sergio'],
-    'left_handed': [1, 0, 0, 0, 1, 0]
-})
+df_athlete = pd.DataFrame(
+    columns=['year', 'date', 'time', 'medal', 'name', 'left_handed'],
+    data=[
+        (2019, '20191103', '145509', 'Gold', 'Wayne', 1),
+        (2019, '20190812', '135433', 'Bronze', 'Robert', 0),
+        (2020, '20200125', '214412', 'Silver', 'Ashley', 0),
+        (2020, '20200129', '124254', 'Bronze', 'Jamie', 0),
+        (2020, '20200412', '123349', 'Silver', 'Jessie', 1),
+        (2020, '20200220', '233517', 'Silver', 'Sergio', 0)
+    ]
+)
 
-df_job = pd.DataFrame({
-    'worker': [
-        'Wayne', 'Robert', 'Ashley',
-        'Jamie', 'Jessie', 'Sergio',
-        'Harry', 'Johnny', 'Aaron'
+df_job = pd.DataFrame(
+    columns=['worker', 'age', 'job', 'years_on_job'],
+    data = [
+        ('Wayne', 8, 'Student', 0),
+        ('Robert', 37, 'Data Scientist', 12),
+        ('Ashley', 25, 'DATA ANALYST', 2),
+        ('Jamie', 26, 'data engineer', 6),
+        ('Jessie', 80, 'Retired', 0),
+        ('Sergio', 30, 'Business Intelligence', 18),
+        ('Harry', 20, 'Student', 12),
+        ('Johnny', 31, 'Data Analyst', 2),
+        ('Aaron', 28, 'AI Engineer', 8),
     ],
-    'age': [8, 37, 25, 26, 80, 30, 20, 31, 28],
-    'job': [
-        'Student', 'Data Scientist', 'DATA ANALYST',
-        'data engineer', 'Retired', 'Business Intelligence',
-        'Student', 'Data Analyst', 'AI Engineer'
-    ],
-    'years_on_job': [0, 12, 2, 6, 0, 18, 12, 2, 8]
-})
+)
 
-df_trade = pd.DataFrame({
-    'year': pd.Series([2017, 2018, 2019, 2020]),
-    'country': pd.Series([
-        'United\nKingdom  ',
-        '  United\nKingdom',
-        'United    Kingdom',
-        ' United Kingdom\n']),
-    'export': pd.Series([5466, 8558, 8435, 8435]),
-    'import': pd.Series([1546, 3546, 2007, 3574])
-})
+df_trade = pd.DataFrame(
+    columns=['year', 'country', 'export', 'import'],
+    data = [
+        (2017, 'United\nKingdom  ', 5466, 1546),
+        (2018, '  United\nKingdom', 8558, 3546),
+        (2019, 'United    Kingdom', 8435, 2007),
+        (2020, ' United Kingdom\n', 8435, 3574)
+    ]
+)
 
-df_shrimp = pd.DataFrame({
-    'date': ['2020-01-01', '2020-01-02', '2020-01-03'],
-    'commodity': ['Shrimp, frozen, chem free', 'Shrimp, frz, chemical-free', 'Prawn, frz, chemical-free'],
-    'price': [10, 13, 14],
-    'unit': ['usd/kg', 'USD/KG', 'USD/kg']
-})
+df_shrimp = pd.DataFrame(
+    columns=['date', 'commodity', 'price', 'unit'],
+    data=[
+        ('2020-01-01', 'Shrimp, frozen, chem free', 10, 'usd/kg'),
+        ('2020-01-02', 'Shrimp, frz, chemical-free', 13, 'USD/KG'),
+        ('2020-01-03', 'Prawn, frz, chemical-free', 14, 'USD/kg')
+    ]
+)
 
-df_info = pd.DataFrame({
-    'customer_id': [3, 423, 5464],
-    'phone': [363334444, 913334444, 123334444],
-    'name': ['Jack', 'James', 'Gabriel'],
-    'information': ['England Male', 'Colombia Male', 'France Female']
-})
+df_info = pd.DataFrame(
+    columns=['customer_id', 'phone', 'name', 'information'],
+    data=[
+        (3, 363334444, 'Jack', 'England Male'),
+        (423, 913334444, 'James', 'Colombia Male'),
+        (5464, 123334444, 'Gabriel', 'France Female')
+    ]
+)
 
-df_football = pd.DataFrame({
-    'first_name': ['Wayne', 'Cristiano', 'Lionel'],
-    'last_name': ['Rooney', 'Ronaldo', 'Messi'],
-    'position': ['Second Striker', 'Left Winger', 'Right Winger']
-})
+df_football = pd.DataFrame(
+    columns=['first_name', 'last_name', 'position'],
+    data=[
+        ('Wayne', 'Rooney', 'Second Striker'),
+        ('Cristiano', 'Ronaldo', 'Left Winger'),
+        ('Lionel', 'Messi', 'Right Winger')
+    ]
+)
 
-df_report = pd.DataFrame({
-    'year': pd.Series([2019, 2019, 2020, 2020, 2020, 2020]),
-    'company': pd.Series(['Pandas', 'Numpy', 'Pandas', 'Numpy', 'Numpy', 'Pandas']),
-    'sales': pd.Series([5466, 8558, 8435, 7280, 9285, 6650]),
-    'profit': pd.Series([1546, 3546, 3574, 3352, 4678, 2007])
-})
+df_report = pd.DataFrame(
+    columns=['year', 'company', 'sales', 'profit'],
+    data=[
+        (2019, 'Pandas', 5466, 1546),
+        (2019, 'Numpy', 8558, 3546),
+        (2020, 'Pandas', 8435, 3574),
+        (2020, 'Numpy', 7280, 3352),
+        (2020, 'Numpy', 9285, 4678),
+        (2020, 'Pandas', 6650, 2007)
+    ]
+)
 
 df_covid_full = pd.DataFrame(
     columns=['country', 'cases', 'deaths', 'recovered', 'area'],
@@ -92,3 +109,15 @@ df_covid_missing = df_covid_full.copy()
 df_covid_missing.loc[[1, 2, 4, 5, 7, 9], 'recovered'] = None
 df_covid_missing.loc[[0, 4], 'deaths'] = None
 df_covid_missing.loc[[3, 8], 'area'] = None
+
+df_student = pd.DataFrame(
+    columns=['student_id', 'gender'],
+    data=[
+        ('010001', 'Male'),
+        ('030001', 'Female'),
+        ('070001', 'Female'),
+        ('080001', 'Female'),
+        ('110001', 'Male'),
+        ('120001', 'Female')
+    ]
+)
