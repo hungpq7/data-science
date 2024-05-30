181e2a042905de39a0c82fa98a7a3bde51411471
hungpq7 <hung.pq8596@gmail.com>
2024-05-30 23:58:58 +0700

    -

--- 05-data-visualization/util/__init__.py
+++ 05-data-visualization/util/__init__.py
@@ -2,3 +2,10 @@ from .config import config_display
 config_display()
 
 from .alias import np, pd, plt, sns
+from .plotting import (
+    pie_chart,
+    line_chart,
+    bar_chart,
+    area_chart,
+    scatter_plot,
+)
