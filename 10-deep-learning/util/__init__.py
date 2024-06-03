7331a19b91c5f297725e0c8311c9a29d77749eb1
Quang Hung Pham <69830292+hungpq7@users.noreply.github.com>
2024-06-03 21:54:08 +0700

    -

---
+++ 10-deep-learning/util/__init__.py
@@ -0,0 +1,7 @@
+from .config import config_display
+config_display()
+
+from .alias import (
+    np, pd, stats,
+    plt, sns,
+)
---
+++ 10-deep-learning/util/alias.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+import janitor
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+import seaborn as sns
