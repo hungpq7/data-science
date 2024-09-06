cddda67a9fbed510d6bb2f91e4f5d9315fd610ee
Quang Hung Pham <69830292+hungpq7@users.noreply.github.com>
2024-09-06 08:57:46 +0700

    -

--- 12-data-science-apps/streamlit/app.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-
-import streamlit as st
-st.set_page_config(layout='wide')
-
-st.sidebar.radio('Approach', ['Fundamental', 'Technical'])
-
-st.write('Hello world')
